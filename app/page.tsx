6cd732b085b2ccfc43fcbeb3fe89425ddbe3eaa5
A_Oliinyk <aolejnik916@gmail.com>
2026-07-17 18:23:41 +0200

    create home page, add fixed header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 import css from './page.module.css';
+import Link from 'next/link';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main className={css.home}>
+      <div className={css.container}>
+        <h1 className={css.title}>Find your perfect rental car</h1>
+        <p className={css.text}>
+          Reliable and budget-friendly rentals for any journey
+        </p>
+        <Link className={css.link} href="/catalog" aria-label="Catalog">
+          View Catalog
+        </Link>
+      </div>
+    </main>
+  );
 }
