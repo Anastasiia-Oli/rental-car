5c19d5e7520d021d712a3d15954f725f6bd97af1
A_Oliinyk <aolejnik916@gmail.com>
2026-07-27 18:06:35 +0200

    add car card markup

--- components/CarList/CarList.tsx
+++ components/CarList/CarList.tsx
@@ -1,5 +1,8 @@
 import css from './CarList.module.css';
 import type { Car } from '@/types/filters.types';
+import { useQuery } from '@tanstack/react-query';
+import { getCars } from '@/lib/api';
+import Link from 'next/link';
 
 type CarListProps = {
   cars: Car[];
@@ -15,8 +18,15 @@ function CarList({ cars }: CarListProps) {
               {car.brand} <span className={css.model}>{car.model}</span>,{' '}
               {car.year}
             </h2>
-            <></>
+            <div className={css.price}>{car.rentalPrice}</div>
           </div>
+          <div className={css.detailsContainer}>
+            {car.location.city} {car.location.country} {car.rentalCompany}{' '}
+            {car.type} {car.mileage}km
+          </div>
+          <Link href={`/catalog/${car.id}`} className={css.detailsBtn}>
+            Read more
+          </Link>
         </li>
       ))}
     </ul>
